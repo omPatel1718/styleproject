e9fd3524488604ff6ca77b639ac0df5badcb9f21
omPatel1718 <67968273+omPatel1718@users.noreply.github.com>
2022-09-16 12:44:33 -0500

    time to break it for fun (otherwise known as adding a second page

---
+++ app/src/main/java/com/example/stylepractice/SignUpLogIn.java
@@ -0,0 +1,14 @@
+package com.example.stylepractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpLogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_log_in);
+    }
+}
